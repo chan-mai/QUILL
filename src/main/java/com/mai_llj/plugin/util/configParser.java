216681cfcb7857dfc8bd7c94a273c5252d8d20d5
mai_llj <yashinomi0804@gmail.com>
2022-12-31 01:24:35 +0900

    add: config.ymlの動的定義部分を置き換える関数を実装

---
+++ src/main/java/com/mai_llj/plugin/util/configParser.java
@@ -0,0 +1,16 @@
+package com.mai_llj.plugin.util;
+
+import static com.mai_llj.plugin.config.config.*;
+
+public class configParser {
+    public static String confParse(String text, String playerName, int x, int y, int z) {
+        // メッセージの置換
+        text = text.replace("%player%", playerName);
+        text = text.replace("%x%", String.valueOf(x));
+        text = text.replace("%y%", String.valueOf(y));
+        text = text.replace("%z%", String.valueOf(z));
+        text = text.replace("%%server_name%%", serverName);
+
+        return text;
+    }
+}
